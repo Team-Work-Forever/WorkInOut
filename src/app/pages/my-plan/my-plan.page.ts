fcc5ba1a62a4de1273b11aa6fa8c75e678befb6a
Davidbraga6 <davidjosebraga@gmail.com>
2023-05-22 18:12:40 +0100

    add: separator with trash to my_plans page

--- src/app/pages/my-plan/my-plan.page.ts
+++ src/app/pages/my-plan/my-plan.page.ts
@@ -1,15 +1,109 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule, NavController } from '@ionic/angular';
+import { CardComponent } from 'src/app/components/card/card.component';
+import { WorkHeaderComponent } from 'src/app/components/work-header/work-header.component';
+import { Card } from 'src/app/interfaces/card.inteface';
+import { loadTourismPoints } from 'src/app/services/api';
 
 @Component({
-  selector: 'app-my-plan',
-  templateUrl: './my-plan.page.html',
-  styleUrls: ['./my-plan.page.scss'],
+    selector: 'app-my-plan',
+    templateUrl: './my-plan.page.html',
+    styleUrls: ['./my-plan.page.scss'],
+    standalone: true,
+    imports: [IonicModule, CommonModule, WorkHeaderComponent, CardComponent],
 })
 export class MyPlanPage implements OnInit {
+    results: Card[];
+    cards: Card[];
 
-  constructor() { }
+    constructor(private nav: NavController) {
+        this.cards = [
+            {
+                title: 'Treino',
+                image: '/assets/workType/alongamentos.png',
+                time: '2 min',
+                list: loadTourismPoints(),
+                isFavorite: true,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+            {
+                title: 'Treino',
+                image: '/assets/workType/cardio.png',
+                time: '1 min',
+                list: loadTourismPoints(),
+                isFavorite: false,
+            },
+        ];
 
-  ngOnInit() {
-  }
+        this.results = this.cards;
+    }
 
+    ngOnInit() {}
+
+    handleClick(card: Card) {
+        // this.nav.navigateForward('/detalhe', { state: card });
+    }
 }

11809f4178872ee529583f53ee345ef3bddba21d
CavasCallahan <dassuncao1234@gmail.com>
2023-05-23 13:58:49 +0100

    add: Supabase Implementation, and some services

---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+interface Exercicio {
+    name: string;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SupabaseService {
+    private supabaseClient: SupabaseClient;
+
+    constructor() {
+        this.supabaseClient = createClient(
+            'https://qbkliymokbvleuaxyiwn.supabase.co',
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia2xpeW1va2J2bGV1YXh5aXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4MzQyOTMsImV4cCI6MjAwMDQxMDI5M30.hJSTjvvVYKh_7kV_PvKppGUkb6cLPdUPzhdI0GBAHe0'
+        );
+    }
+
+    public getClient(): SupabaseClient {
+        return this.supabaseClient;
+    }
+}

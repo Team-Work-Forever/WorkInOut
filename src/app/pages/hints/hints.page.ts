ee4c95ad1299304d3fed587ea52b4f94c0695608
Davidbraga6 <davidjosebraga@gmail.com>
2023-05-24 11:47:43 +0100

    add: routing to hints

---
+++ src/app/pages/hints/hints.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hints',
+  templateUrl: './hints.page.html',
+  styleUrls: ['./hints.page.scss'],
+})
+export class HintsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
